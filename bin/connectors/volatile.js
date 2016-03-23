d718d0c5025606f44704a685c28af381b7b09e40
James Speirs <james.speirs@gmail.com>
2016-03-23 15:42:50 -0600

    update connector config

--- bin/connectors/volatile.js
+++ bin/connectors/volatile.js
@@ -1,42 +1,35 @@
 "use strict";
-// A volatile in memory database for key value pairs. On disconnect all data is lost.
+// A volatile in memory database for key value pairs. After request all data is lost.
 
-var Connector           = require('../database/connector');
-var is                  = require('../util/is');
-var noop                = require('../util/noop');
+module.exports = {
+    name: 'volatile',
+    pool: false,
+    connect: function connect() {
+        var client;
+        var manager;
+        var store;
 
-var allStores = {};
+        client = {
+            del: (key) => delete store[key],
+            get: (key) => store[key],
+            exists: (key) => store.hasOwnProperty(key),
+            list: () => Object.keys(store),
+            set: (key, value) => store[key] = value,
+            wipe: () => store = {}
+        };
 
-Connector.define({
-    name: 'volatile',                               // the name of the connector
-    pool: false,                                    // specify that the pool manager will not be used with this connector
-    connect: connect,                               // specify how to get a connection
-    options: {}                                     // configuration options specific to this connector
-});
+        manager = {
+            disconnect: () => store = void 0,
+            preRequest: () => store = {},
+            postRequest: () => store = void 0,
+            query: (key) => client.get(key)
+        };
 
-function connect(config) {
-    var client;
-    var manager;
-    var store;
+        return {
+            client: client,
+            manager: manager
+        }
+    },
+    options: {}
+};
 
-    client = {
-        del: (key) => delete store[key],
-        get: (key) => store[key],
-        exists: (key) => store.hasOwnProperty(key),
-        list: () => Object.keys(store),
-        set: (key, value) => store[key] = value,
-        wipe: () => store = {}
-    };
-
-    manager = {
-        disconnect: () => store = void 0,
-        preRequest: () => store = {},
-        postRequest: () => store = void 0,
-        query: (key) => client.get(key)
-    };
-
-    return {
-        client: client,
-        manager: manager
-    }
-}
